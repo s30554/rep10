b993ededeabcf4427a28c9b8191f5cbc54928d17
s30554 <162422182+s30554@users.noreply.github.com>
2024-06-04 16:39:01 +0200

    Add files via upload

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+public class Employee extends Person {
+
+    private int hireDate;
+    private String companyName;
+    public double salary;
+
+    public Employee(String firstName, String lastName, int birthdayYear, int hireDate, String companyName, double salary) {
+        super(firstName, lastName, birthdayYear);
+        this.hireDate = hireDate;
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public int getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(int hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getJobSeniority(){
+        int jobSeniority = 2024 - this.hireDate;
+        return jobSeniority;
+    }
+    public double salary(){
+        return salary;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,40 @@
+public abstract class Person {
+    private String firstName;
+     String lastName;
+    private int birthdayYear;
+
+    public Person(String firstName, String lastName, int birthdayYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayYear = birthdayYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthdayYear() {
+        return birthdayYear;
+    }
+
+    public void setBirthdayYear(int birthdayYear) {
+        this.birthdayYear = birthdayYear;
+    }
+
+    public int getAge() {
+        int Age = 2024 - this.birthdayYear;
+        return Age;
+    }
+}
